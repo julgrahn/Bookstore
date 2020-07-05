49bf5788ab5cf3388a8ed965d2551e093fd07cc4
julgrahn <alexander.grahn@hotmail.se>
2020-07-05 19:49:52 +0200

    add get selected row function

--- frontend.py
+++ frontend.py
@@ -1,6 +1,19 @@
 from tkinter import *
 import backend
 
+def getSelectedRow(event):
+    global selectedTuple
+    index = list1.curselection()[0]
+    selectedTuple = list1.get(index)
+    e1.delete(0, END)
+    e1.insert(END, selectedTuple[1])
+    e2.delete(0, END)
+    e2.insert(END, selectedTuple[2])
+    e3.delete(0, END)
+    e3.insert(END, selectedTuple[3])
+    e4.delete(0, END)
+    e4.insert(END, selectedTuple[4])
+
 def viewCommand():
     list1.delete(0, END)
     for row in backend.view():
@@ -15,6 +28,13 @@ def addEntry():
     backend.insert(titleText.get(), authorText.get(), yearText.get(), isbnText.get())
     list1.delete(0, END)
     list1.insert(END, (titleText.get(), authorText.get(), yearText.get(), isbnText.get()))
+
+def deleteEntry():
+    backend.delete(selectedTuple[0])
+
+def updateEntry():
+    backend.update(selectedTuple[0], selectedTuple[1], selectedTuple[2], selectedTuple[3], selectedTuple[4])
+    print(selectedTuple[0], selectedTuple[1], selectedTuple[2], selectedTuple[3], selectedTuple[4])
 
 window = Tk()
 
@@ -41,16 +61,18 @@ e2.grid(row = 0, column = 3)
 
 
 yearText = StringVar()
-e2 = Entry(window, textvariable = yearText)
-e2.grid(row = 1, column = 1)
+e3 = Entry(window, textvariable = yearText)
+e3.grid(row = 1, column = 1)
 
 
 isbnText = StringVar()
-e2 = Entry(window, textvariable = isbnText)
-e2.grid(row = 1, column = 3)
+e4 = Entry(window, textvariable = isbnText)
+e4.grid(row = 1, column = 3)
 
 list1 = Listbox(window, height = 6, width = 35)
 list1.grid(row = 2, column = 0, rowspan = 6, columnspan = 2)
+
+list1.bind('<<ListboxSelect>>', getSelectedRow)
 
 sb1 = Scrollbar(window)
 sb1.grid(row = 2, column = 2, rowspan = 6)
@@ -70,11 +92,11 @@ b3 = Button(window, text = "Add Entry", width = 12, command = addEntry)
 b3.grid(row = 4, column = 3)
 
 
-b4 = Button(window, text = "Update Selected", width = 12)
+b4 = Button(window, text = "Update Selected", width = 12, command = updateEntry)
 b4.grid(row = 5, column = 3)
 
 
-b5 = Button(window, text = "Delete Selected", width = 12)
+b5 = Button(window, text = "Delete Selected", width = 12, command = deleteEntry)
 b5.grid(row = 6, column = 3)
 
 
